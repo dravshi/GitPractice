e926c80bc846fd2d0a4e972fac4dec0b039ec109
shishir <dravid.shishir1990@gmail.com>
2024-05-03 22:41:08 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+
+public class Calculator {
+    public static void main(String[] args) {
+        int c = add(5,6);
+        System.out.println("Addition result:"+c);
+    }
+    public static int add (int a, int b) {
+        return a + b;
+    }
+}
