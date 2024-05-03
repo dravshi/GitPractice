42cf731fb5ea4d1888b28c577f4cfa45073561ae
shishir <dravid.shishir1990@gmail.com>
2024-05-03 22:42:19 +0200

    Added subtract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,8 +3,15 @@ public class Calculator {
     public static void main(String[] args) {
         int c = add(5,6);
         System.out.println("Addition result:"+c);
+
+        c = sub(5,6);
+        System.out.println("Subtraction result:"+c);
     }
     public static int add (int a, int b) {
         return a + b;
+    }
+
+    public static int sub (int a, int b) {
+        return a - b;
     }
 }
